aa30f758ced715209bcb40bcb114b53febd6f9b7
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:30:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,12 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    session[:name]
+  end
 
+  private
+
+  def require_login
+    redirect_to controller: 'sessions', action: 'new' unless current_user
   end
 end

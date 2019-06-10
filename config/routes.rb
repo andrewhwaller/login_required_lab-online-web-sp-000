657b2b65d47c2597ca733c5b978e2b5991d4fbcf
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:32:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/current_user'
+  get 'secrets/new'
+
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
